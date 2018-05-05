ff884942dd3ce8d6dcef07f400d8cd49506b13eb
McTarkus <38666790+McTarkus@users.noreply.github.com>
2018-05-05 16:08:07 -0400

    Initialize Project (just copying in Exam 3 stuff now)

---
+++ app/src/main/java/edu/rosehulman/alumbajt/exam7coneseeking/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.rosehulman.alumbajt.exam7coneseeking;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
