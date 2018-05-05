9e6d2944b241af5f4f0cc420d4f413c3a9445773
McTarkus <38666790+McTarkus@users.noreply.github.com>
2018-05-05 16:33:53 -0400

    Got rid of error codes, still working on updating the app for lab

--- app/src/main/java/edu/rosehulman/alumbajt/exam7coneseeking/MainActivity.java
+++ app/src/main/java/edu/rosehulman/alumbajt/exam7coneseeking/MainActivity.java
@@ -22,13 +22,9 @@ public class MainActivity extends AccessoryActivity implements FieldGpsListener 
     private TextView mTargetXYTextView;
     private TextView mTurnAmountTextView;
     private TextView mCommandTextView;
-    private TextView mLeftBallTextView;
-    private TextView mMiddleBallTextView;
-    private TextView mRightBallTextView;
     private TextView mTargetHeadingTextView;
 
     private long mStateStartTime;
-    private long mSubStateTime;
     private int mGPSCounter = 0;
     private double mCurrentGPSX;
     private double mCurrentGPSY;
@@ -51,22 +47,12 @@ public class MainActivity extends AccessoryActivity implements FieldGpsListener 
     enum State {
         READY_FOR_MISSION,
         INITIAL_STRAIGHT,
-        NEAR_BALL_MISSION,
-        FAR_BALL_MISSION,
-        HOME_CONE_MISSION,
-        WAITING_FOR_PICKUP,
-        SEEKING_HOME
+        GPS_SEEKING,
+        BALL_REMOVAL_SCRIPT
     }
 
-    enum subState {
-        INACTIVE,
-        GPS_SEEKING,
-        IMAGE_REC_SEEKING,
-        OPTIONAL_SCRIPT
-    }
 
     private State mState = State.READY_FOR_MISSION;
-    private subState mSubState = subState.INACTIVE;
     private Timer mTimer;
     public static final int LOOP_INTERVAL_MS = 100;
     private Handler mCommandHandler = new Handler();
@@ -76,18 +62,13 @@ public class MainActivity extends AccessoryActivity implements FieldGpsListener 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setState(State.READY_FOR_MISSION);
-        setSubState(subState.INACTIVE);
 
         mHighLevelStateTextView = findViewById(R.id.highLevelSubstateTextView);
-        mMissionSubStateTextView = findViewById(R.id.missionSubstateTextView);
         mGPSTextView = findViewById(R.id.gPSTextView);
         mTargetXYTextView = findViewById(R.id.targetXYTextView);
         mTargetHeadingTextView = findViewById(R.id.targetHeadingTextView);
         mTurnAmountTextView = findViewById(R.id.turnAmountTextView);
         mCommandTextView = findViewById(R.id.commandTextView);
-        mLeftBallTextView = findViewById(R.id.leftBallTextView);
-        mMiddleBallTextView = findViewById(R.id.middleBallTextView);
-        mRightBallTextView = findViewById(R.id.rightBallTextView);
     }
 
     @Override
@@ -116,170 +97,69 @@ public class MainActivity extends AccessoryActivity implements FieldGpsListener 
     public void loop() {
         mHighLevelStateTextView.setText(mState.name() + " " + getStateTimeMS() / 1000);
 
-        int leftSpeed = DEFAULT_TURN;
-        int rightSpeed = DEFAULT_TURN;
+        int leftSpeed;
+        int rightSpeed;
 
-        if (mSubState == subState.INACTIVE) {
-            mMissionSubStateTextView.setText("---");
-        } else {
-            mMissionSubStateTextView.setText(mSubState.name() + " " + getSubStateTimeMS() / 1000);
-        }
         switch (mState) {
-
             case READY_FOR_MISSION:
                 break;
             case INITIAL_STRAIGHT:
                 break;
-            case NEAR_BALL_MISSION:
-                break;
-            case FAR_BALL_MISSION:
-                break;
-            case HOME_CONE_MISSION:
-                break;
-            case WAITING_FOR_PICKUP:
-                if (getStateTimeMS() > 5000) {
-                    setState(State.SEEKING_HOME);
-                }
-                break;
-            case SEEKING_HOME:
-                if (getStateTimeMS() > 5000) {
-                    setState(State.WAITING_FOR_PICKUP);
-                }
-                break;
-        }
-
-        switch (mSubState) {
-
-            case INACTIVE:
-
-                mTargetHeadingTextView.setText("---");
-                mTurnAmountTextView.setText("---");
-                mCommandTextView.setText("---");
-                break;
             case GPS_SEEKING:
-                if (getSubStateTimeMS() > 15000) {
-                    setSubState(subState.OPTIONAL_SCRIPT);
-                }
-                mTargetHeadingTextView.setText( Math.round(NavUtils.getTargetHeading(mCurrentGPSX, mCurrentGPSY, mTargetX, mTargetY)) + "°");
-
                 double leftTurnAmount = Math.round(NavUtils.getLeftTurnHeadingDelta(mCurrentHeading, NavUtils.getTargetHeading(mCurrentGPSX, mCurrentGPSY, mTargetX, mTargetY)));
                 double rightTurnAmount = Math.round(NavUtils.getRightTurnHeadingDelta(mCurrentHeading, NavUtils.getTargetHeading(mCurrentGPSX, mCurrentGPSY, mTargetX, mTargetY)));
                 if (NavUtils.targetIsOnLeft(mCurrentGPSX, mCurrentGPSY, mCurrentHeading, mTargetX, mTargetY)) {
-                    mTurnAmountTextView.setText("Left " + (int)leftTurnAmount + "°");
+                    mTurnAmountTextView.setText("Left " + (int) leftTurnAmount + "°");
                     leftSpeed = DEFAULT_TURN - (int) leftTurnAmount;
                     rightSpeed = DEFAULT_TURN;
                 } else {
-                    mTurnAmountTextView.setText("Right " + (int)rightTurnAmount  + "°");
+                    mTurnAmountTextView.setText("Right " + (int) rightTurnAmount + "°");
                     leftSpeed = DEFAULT_TURN;
-                    rightSpeed = DEFAULT_TURN - (int)rightTurnAmount;
+                    rightSpeed = DEFAULT_TURN - (int) rightTurnAmount;
                 }
-
-
                 mCommandTextView.setText("WHEEL SPEED FORWARD " + leftSpeed + " FORWARD " + rightSpeed);
-
-
-
                 break;
-            case IMAGE_REC_SEEKING:
-                mTargetHeadingTextView.setText("---");
-                mTurnAmountTextView.setText("---");
-                mCommandTextView.setText("---");
-                break;
-            case OPTIONAL_SCRIPT:
-
-                mTargetHeadingTextView.setText("---");
-                mTurnAmountTextView.setText("---");
-                mCommandTextView.setText("---");
-                if (mState == State.NEAR_BALL_MISSION) {
-                    if (getSubStateTimeMS() > 4000) {
-                        setState(State.FAR_BALL_MISSION);
-                        mLeftBallTextView.setText("---");
-                        updateTargetXY(240, 50);
-                        onLocationChanged(90, -50, -46, null);
-                    }
-                }
-                if (mState == State.FAR_BALL_MISSION) {
-                    if (getSubStateTimeMS() > 4000) {
-                        mRightBallTextView.setText("---");
-                    }
-                    if (getSubStateTimeMS() > 8000) {
-                        mMiddleBallTextView.setText("---");
-                        setState(mState.HOME_CONE_MISSION);
-                        updateTargetXY(0, 0);
-                        onLocationChanged(240, 50, 45, null);
-                    }
-                }
-                if (mState == State.HOME_CONE_MISSION) {
-                    if (getSubStateTimeMS() > 1000) {
-                        setState(State.WAITING_FOR_PICKUP);
-                    }
-                }
-                if (mState == State.SEEKING_HOME) {
-                    if (getStateTimeMS() > 5000) {
-                        setState(State.WAITING_FOR_PICKUP);
-                    }
-                }
+            case BALL_REMOVAL_SCRIPT:
+                //TODO: Figure out ball removal script
                 break;
         }
+
+
     }
+
 
     private long getStateTimeMS() {
         return System.currentTimeMillis() - mStateStartTime;
     }
 
-    private long getSubStateTimeMS() {
-        return System.currentTimeMillis() - mSubStateTime;
-    }
 
     private void setState(State newState) {
         mStateStartTime = System.currentTimeMillis();
         switch (newState) {
             case READY_FOR_MISSION:
-                setSubState(subState.INACTIVE);
                 break;
             case INITIAL_STRAIGHT:
-                setSubState(subState.INACTIVE);
                 mCommandHandler.postDelayed(new Runnable() {
                     @Override
                     public void run() {
-                        setState(State.NEAR_BALL_MISSION);
+                        setState(State.GPS_SEEKING);
                         onLocationChanged(60, -25, -30, null);
                     }
                 }, 4000);
                 break;
-            case NEAR_BALL_MISSION:
-                setSubState(subState.GPS_SEEKING);
+            case GPS_SEEKING:
                 break;
-            case FAR_BALL_MISSION:
-                setSubState(subState.GPS_SEEKING);
-                break;
-            case HOME_CONE_MISSION:
-                setSubState(subState.GPS_SEEKING);
-                break;
-            case WAITING_FOR_PICKUP:
-                setSubState(subState.INACTIVE);
-                break;
-            case SEEKING_HOME:
-                setSubState(subState.GPS_SEEKING);
+            case BALL_REMOVAL_SCRIPT:
                 break;
         }
-
 
         mState = newState;
     }
 
-    private void setSubState(subState newSubState) {
-        mSubStateTime = System.currentTimeMillis();
-
-        mSubState = newSubState;
-    }
 
     public void handleReset(View view) {
 //        Toast.makeText(this, "You pressed RESET", Toast.LENGTH_SHORT).show();
         setState(State.READY_FOR_MISSION);
-        mLeftBallTextView.setText("Red\nBall");
-        mMiddleBallTextView.setText("White\nBall");
-        mRightBallTextView.setText("Blue\nBall");
         mTargetXYTextView.setText("---");
         mGPSCounter = 0;
         mGPSTextView.setText("---");
@@ -297,36 +177,11 @@ public class MainActivity extends AccessoryActivity implements FieldGpsListener 
         }
     }
 
-    public void handleNotSeen(View view) {
-//        Toast.makeText(this, "You pressed NOT SEEN", Toast.LENGTH_SHORT).show();
-
-        if (mSubState == subState.IMAGE_REC_SEEKING) {
-            setSubState(subState.GPS_SEEKING);
-        }
-    }
-
-    public void handleSeenSmall(View view) {
-//        Toast.makeText(this, "You pressed SEEN SMALL", Toast.LENGTH_SHORT).show();
-
-        if (mSubState == subState.GPS_SEEKING) {
-            setSubState(subState.IMAGE_REC_SEEKING);
-        }
-    }
-
-    public void handleSeenBig(View view) {
-//        Toast.makeText(this, "You pressed SEEN BIG", Toast.LENGTH_SHORT).show();
-        if (mSubState == subState.IMAGE_REC_SEEKING) {
-            setSubState(subState.OPTIONAL_SCRIPT);
-        }
-    }
 
     public void handleMissionComplete(View view) {
 //        Toast.makeText(this, "You pressed MISSION COMPLETE", Toast.LENGTH_SHORT).show();
-        if (mState == State.WAITING_FOR_PICKUP) {
+        if (mState == State.GPS_SEEKING) {
             setState(mState.READY_FOR_MISSION);
-            mLeftBallTextView.setText("Red\nBall");
-            mMiddleBallTextView.setText("White\nBall");
-            mRightBallTextView.setText("Blue\nBall");
             mTargetXYTextView.setText("---");
             mGPSCounter = 0;
             mGPSTextView.setText("---");
